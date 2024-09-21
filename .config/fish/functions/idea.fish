6ed1b1b1c5a6deb55f153ab020f594a04baad036
dimanu-py <diegomtz126@gmail.com>
2024-09-21 15:51:40 +0200

    feat(fish): update fish config files

--- .config/fish/functions/idea.fish
+++
@@ -1,3 +0,0 @@
-function idea
-    /Users/dimanu/Library/Application\ Support/JetBrains/Toolbox/scripts/idea
-end
--- .config/fish/functions/pycharm.fish
+++
@@ -1,3 +0,0 @@
-function pycharm
-    /Users/dimanu/Library/Application\ Support/JetBrains/Toolbox/scripts/pycharm
-end
