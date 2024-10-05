390395da3fb12763f69fdfd02e5f9d2e9fb2569f
dimanu-py <diegomtz126@gmail.com>
2024-10-06 01:36:24 +0200

    feat(monitors): add monitors configuration scripts

---
+++ monitors/.config/monitors/drivers_installation.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+yay -S -y evdi displaylink
+echo "Installed evdi and displaylink successfully"
+
+systemctl enable displaylink.service
+echo "Displaylink enable"
+
+file_path="/etc/X11/xorg.conf.d/20-evdi.conf"
+
+if [ ! -d "$(dirname "$file_path")"  ]; then
+	sudo mkdir -p "$(dirname "$file_path")"
+fi
+
+sudo tee "$file_path" > /dev/null << EOF
+Section "OutputClass"
+	Identifier "DisplayLink"
+	MatchDriver "evdi"
+	Driver "modesetting"
+	Option "AccelMethod" "none"
+EndSection
+EOF
+echo "Configuration written to $file_path"
